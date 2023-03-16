679fb36d43b16d87c2e25941362adbfeb7d262d6
henryofjupiter <oshardhenry@gmail.com>
2023-03-16 06:36:34 -0400

    adds functions formatTime24 and formatTime12 that returns a string representation of the 24 & 12hr format of time.

--- clockapp.cpp
+++ clockapp.cpp
@@ -12,7 +12,6 @@ string nCharString(size_t n, char c) {
     return newString;
 }
 
-//make commit before proceeding
 string twoDigitString(unsigned int n) {
     string newString;
 
@@ -29,13 +28,50 @@ string twoDigitString(unsigned int n) {
     return newString;
 }
 
+//remember to make commit before proceeding
+string formatTime24(unsigned int h, unsigned int m, unsigned int s) {
+    string newString;
+
+    newString = twoDigitString(h) + ":" + twoDigitString(m) + ":" + twoDigitString(s);
+    return newString;
+}
+
+string formatTime12(unsigned int h, unsigned int m, unsigned int s) {
+    // FIX ME: excessive conditions
+
+    string newString;
+    if (h == 0) {
+        newString = twoDigitString(h + 12) + ":" + twoDigitString(m) + ":" + twoDigitString(s) + " A M";
+    }
+    if (h >= 1 && h < 12) {
+        newString = twoDigitString(h) + ":" + twoDigitString(m) + ":" + twoDigitString(s) + " A M";
+
+    }
+    if (h >= 12 && h <= 24) {
+        if (h > 12) {
+            newString = twoDigitString(h - 12) + ":" + twoDigitString(m) + ":" + twoDigitString(s) + " P M";
+        }
+        else {
+            newString = twoDigitString(h) + ":" + twoDigitString(m) + ":" + twoDigitString(s) + " P M";
+        }
+    }
+    return newString;
+}
+
+
+
 int main() {
-    const int n = 5;
-    const char c = '*';
-//make commit before proceeding
+    const int n = 5;   //for testing
+    const char c = '*'; //for testing
+    int h, m, s;        //for testing
+
     int userInput;
 
     cin >> userInput;
     cout << nCharString(n, c) << endl;
     cout << twoDigitString(userInput) << endl;
+    cin >> h >> m >> s;
+    cout << formatTime24(h, m, s) << endl;
+    cout << formatTime12(h, m, s) << endl;
+
 }
