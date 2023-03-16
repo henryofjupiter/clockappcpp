5eca4025ad1790c81472dbb32129925701ef633f
henryofjupiter <oshardhenry@gmail.com>
2023-03-16 00:17:16 -0400

    adds function for twoDigitString in order to format single digit numbers from 0-9 into 00-09

--- clockapp.cpp
+++ clockapp.cpp
@@ -12,9 +12,30 @@ string nCharString(size_t n, char c) {
     return newString;
 }
 
+//make commit before proceeding
+string twoDigitString(unsigned int n) {
+    string newString;
+
+    for (int i = 0; i < 60; ++i) {
+        if (n >= 0 && n <= 9) {
+            if (n == i) {
+                newString = "0" + to_string(n);
+            }
+        }
+        else {
+            newString = to_string(n);
+        }
+    }
+    return newString;
+}
+
 int main() {
     const int n = 5;
     const char c = '*';
+//make commit before proceeding
+    int userInput;
 
+    cin >> userInput;
     cout << nCharString(n, c) << endl;
+    cout << twoDigitString(userInput) << endl;
 }
